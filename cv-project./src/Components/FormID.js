d110b47bc2e5b21e1d57db10e03787efd0ea4b6d
amaury <tissot.amaury@gmail.com>
2022-07-12 14:46:56 +0200

    Add FormId

---
+++ cv-project./src/Components/FormID.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class FormID {
+
+    DisplayForm () {
+
+        return(
+            <div>
+            <form>
+                <label htmlFor="FirstName">First Name
+                    <input>
+                    </input>
+                </label>
+                
+    
+                <button>
+    
+                </button>
+            </form>
+            </div>
+        )
+    }
+
+    
+
+}
+export default FormID;
